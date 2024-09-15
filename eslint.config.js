2714a8ffa5ad24ba8d2e78af0c7a88ff6891072e
Arfelo Reed Torralba <CPAMaster1994@gmail.com>
2024-09-15 22:19:42 +0800

    finished hero and added about section

--- eslint.config.js
+++ eslint.config.js
@@ -11,6 +11,7 @@ import react from "eslint-plugin-react";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
+    settings: { react: { version: "18.3" } },
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
@@ -42,6 +43,8 @@ export default tseslint.config(
       // added for production development
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
+      // my personal setup
+      "@typescript-eslint/no-unused-vars": ["warn"],
     },
   },
 );
